f03eb9905b1cd0eebe3261e875f3439d5292f67e
emil.f21 <emil.f21@P-0G-0B121-0097>
2026-02-09 12:49:16 +0100

    git commit -m "mit den Tests die Fehler behoben"

--- 13.3_tdd_Geometrie/Rechteck.cs
+++ 13.3_tdd_Geometrie/Rechteck.cs
@@ -14,13 +14,37 @@ namespace _13._3_tdd_Geometrie
         private int hoehe;
 
         public int Breite { 
-            get { return breite; } 
-            set { breite = value; }
+            get { return breite;} 
+            set 
+            { if (value > 0)
+                {
+                    breite = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Wert zu klein");
+                }
+            }
             
         }
         public int Hoehe { 
-            get { return hoehe; } 
-            set { hoehe = value; }
+            get { return hoehe; }
+            set
+            {
+                if (value > 0)
+                {
+                    hoehe = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Wert zu klein");
+                }
+            }
+        }
+
+        public int Umfang()
+        {
+            return 2 * hoehe + 2 * breite;
         }
 
         public void Skalieren(double faktor)
@@ -31,6 +55,22 @@ namespace _13._3_tdd_Geometrie
             }
             breite = Convert.ToInt32(breite * faktor);
             hoehe = Convert.ToInt32(hoehe * faktor);
+        }
+
+        public int Flaeche ()
+        {
+            return breite * hoehe;
+        }
+
+        public Rechteck(int breite, int hoehe)
+        {
+            this.breite = breite;
+            this.hoehe = hoehe;
+        }
+
+        public Rechteck()
+        {
+
         }
     }
 }
